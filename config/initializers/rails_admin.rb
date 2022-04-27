7d6283aa5bcb0e0ba21699148167360c0531f2d4
Andres Gualde <andres@gualde.com>
2022-04-27 21:22:06 +0200

    Restrict admin access to User and Product model

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,16 +3,20 @@ RailsAdmin.config do |config|
   ### Popular gems integration
 
   ## == Devise ==
-  config.authenticate_with do 
-    warden.authenticate!
-     scope: :user 
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+
+  config.included_models = [ "Product", "User" ]
+
+  config.current_user_method(&:current_user)
+
+  config.authorize_with do
+    unless current_user.admin?
+      flash[:alert] = 'Sorry, no admin access for you.'
+      redirect_to main_app.root_path
     end
-    
-    config.current_user_method(&:current_user) config.authorize_with do 
-      unless current_user.admin? flash[:alert] = 'Sorry, no admin access for you.' 
-        redirect_to main_app.root_path 
-      end 
-    end
+  end
 
   ## == CancanCan ==
   # config.authorize_with :cancancan
