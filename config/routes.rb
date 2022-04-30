b4c0a078b3ae823521e64315a93d36d66a9b7c9c
Andres Gualde <andres@gualde.com>
2022-04-30 23:56:07 +0200

    Centering and styling form btns

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
+  root to: 'products#index'
+
   resources :products, only: [:index, :show]
   resources :order_items
   resource :carts, only:[:show]
 
-  root to: 'products#index'
 
 end
  
