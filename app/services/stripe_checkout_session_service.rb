af1593944af4af06761ffbaa3a95f208110d04c5
Andres Gualde <andres@gualde.com>
2022-05-03 16:54:56 +0200

    Fixing bug on service

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,7 +1,6 @@
 class StripeCheckoutSessionService
     def call(event)
       checkout = Checkout.find_by(checkout_session_id: event.data.object.id)
-      session[:order_id] = nil
       checkout.update(state: 'paid')
     end
   end
