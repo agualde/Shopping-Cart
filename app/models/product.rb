38aeb19fce7bc2be3c140efd7d5bd352574de26b
Andres Gualde <andres@gualde.com>
2022-05-03 12:38:46 +0200

    Product and OrderItem dependence destroyed implemented

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-    has_many :order_items
+    has_many :order_items, dependent: :destroy
     validates :name, :code, :img_url, :price, presence: true
     validates :code, uniqueness: true
 
