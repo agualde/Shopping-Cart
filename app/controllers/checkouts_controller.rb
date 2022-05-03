1c2687fa8420025ac765d31eab5d62fc63aa04a9
Andres Gualde <andres@gualde.com>
2022-05-03 17:28:29 +0200

    Clear session cookies for new cart after payment

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -13,7 +13,7 @@ class CheckoutsController < ApplicationController
     amount_cents = (order.subtotal * 100)
     checkout  = Checkout.create!(amount: order.subtotal, state: 'pending', user: current_user, orders_id: order.id)
 
-    session = Stripe::Checkout::Session.create(
+    stripe_session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [{
         name: 'Test',
@@ -26,7 +26,8 @@ class CheckoutsController < ApplicationController
         cancel_url: checkout_url(checkout)
       )
       
-      checkout.update(checkout_session_id: session.id)
+      checkout.update(checkout_session_id: stripe_session.id)
+      session[:order_id] = nil
       redirect_to new_checkout_payments_path(checkout, checkout_id: checkout)
   end
 end
