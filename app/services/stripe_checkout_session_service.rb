e7aac49ea4539f2b7890fbde10e4be8b9a6280a2
Andres Gualde <andres@gualde.com>
2022-05-03 16:50:54 +0200

    Service update

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,6 +1,7 @@
 class StripeCheckoutSessionService
     def call(event)
       checkout = Checkout.find_by(checkout_session_id: event.data.object.id)
+      session[:order_id] = nil
       checkout.update(state: 'paid')
     end
   end
