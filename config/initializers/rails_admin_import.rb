af1ea9a20e119590f3b61056633726993c1a95bc
Andres Gualde <andres@gualde.com>
2022-05-04 13:09:23 +0200

    Admin import implemented, admin user can now import a CSV JSON or XML file from the admin panel to generate Product models

---
+++ config/initializers/rails_admin_import.rb
@@ -0,0 +1,9 @@
+RailsAdmin.config do |config|
+    # REQUIRED:
+    # Include the import action
+    # See https://github.com/sferik/rails_admin/wiki/Actions
+    config.actions do
+      all
+      import
+    end
+end
