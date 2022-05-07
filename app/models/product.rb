5824422f962d95a3d6797f0e466f4d3f76e31f54
Andres Gualde <andres@gualde.com>
2022-05-07 20:48:22 +0200

    Added home styling and pseudo categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,14 @@
 class Product < ApplicationRecord
     has_many :order_items, dependent: :destroy
+    belongs_to :category, optional: true
     validates :name, :code, :img_url, :price, presence: true
     validates :code, uniqueness: true
 
     include PgSearch::Model
     pg_search_scope :global_search,
-    against: [ :name, :code ],
-    associated_against: {
+    against: [ :name, :code, :tags ],
+    associated_against: { 
+      category: [:name]
     },
       using: {
         tsearch: { prefix: true }
