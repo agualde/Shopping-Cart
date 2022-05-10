b82d330647bfbefa82533ed41672444429f37be1
Andres Gualde <andres@gualde.com>
2022-05-10 23:16:51 +0200

    Front-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 puts 'Cleaning database .........'
-Checkout.destroy_all
 Order.destroy_all
 OrderItem.destroy_all
 Product.destroy_all
-User.destroy_all
 Category.destroy_all
 
 puts 'Creating categories............'
